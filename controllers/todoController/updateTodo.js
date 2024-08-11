70630593aa29c3c39b50b47e789a585ca7b25550
Mahima <mahijain8376@gmail.com>
2024-08-11 20:37:29 +0530

    add updateTodo API

--- controllers/todoController/updateTodo.js
+++ controllers/todoController/updateTodo.js
@@ -1,5 +1,20 @@
-const updateTodo = (req, res, next) => {
+const Todo = require("../../models/todo");
+
+const updateTodo = async (req, res, next) => {
     try {
+        const user = req.user.id;
+        const TodoId = req.params.id;
+        const { title, description, state } = req.body;
+        console.log("User id--", user);
+        console.log("Todo Id--", TodoId);
+        const todo = await Todo.findOne({ user, _id: TodoId });
+
+        todo.title = title || todo.title;
+        todo.description = description || todo.description;
+        todo.state = state || todo.state;
+
+        todo.save();
+        res.status(200).json({ todo, message: "Todo update successfully." })
 
     } catch (error) {
         next(error);
