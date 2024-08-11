e32c135b9830bb3d0ab0e4aeaeb4007df1ad12c7
Mahima <mahijain8376@gmail.com>
2024-08-11 18:03:38 +0530

    update consoles.

--- controllers/authController/signUp.js
+++ controllers/authController/signUp.js
@@ -3,9 +3,10 @@ const jwt = require("jsonwebtoken");
 
 const signUp = async (req, res, next) => {
     try {
+        console.log("Inside signup");
         const { username, password } = req.body;
         let user = await User.findOne({ username });
-        console.log(user);
+
         if (user) {
             const error = new Error("User already exists");
             error.statusCode = 400;
@@ -13,9 +14,11 @@ const signUp = async (req, res, next) => {
         }
 
         user = await User.create({ username, password });
+        console.log("User created.")
 
         const payload = { user: { id: user.id } };
         const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1h' })
+        console.log("Token generated");
         res.status(201).json({ token });
 
     } catch (error) {
