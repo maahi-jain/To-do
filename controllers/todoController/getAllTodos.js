906e073a085fc40dc1478cd72ee831525792bf2e
Mahima <mahijain8376@gmail.com>
2024-08-11 20:14:29 +0530

    Add getAllTodo API

--- controllers/todoController/getAllTodos.js
+++ controllers/todoController/getAllTodos.js
@@ -1,5 +1,10 @@
-const getAllTodos = (req, res, next) => {
+const todo = require("../../models/todo");
+
+const getAllTodos = async (req, res, next) => {
     try {
+        const user = req.user.id;
+        const todos = await todo.find({ user });
+        res.status(200).json({ todos })
 
     } catch (error) {
         next(error);
